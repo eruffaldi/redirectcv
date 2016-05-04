108c151de4701c28b55c8c5ce8336d1062cdd8bc
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-05-04 14:31:46 +0200

    Update replacer.cpp

--- replacer.cpp
+++ replacer.cpp
@@ -5,7 +5,6 @@
 namespace cv
 {
 	// TODO use some C++11 metaprogramming and extract it from the real imshow
-#ifdef PASSSTRING
 	using FX = void(const std::string,InputArray);
 	FX* realimshow;
 	void imshow(const std::string name,InputArray p)
@@ -13,25 +12,18 @@ namespace cv
 		if(name[0] == '!')
 			 realimshow(name.substr(1),p);
 	}
-#else
-	using FX = void(const char *, InputArray);
-	FX* realimshow;
-	void imshow(const char* name,InputArray p)
-	{
-		if(name[0] == '!')
-			realimshow(name+1,p);
-	}
-#endif
-
 
 	void entrypoint()
 	{
 		// nm -gU libopencv_highgui.dylib  | grep imshow
-#ifdef PASSSTRING
-		realimshow = (FX*)dlsym(RTLD_NEXT,"__ZN2cv6imshowERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEERKNS_11_InputArrayE");
-#else
-		realimshow = (FX*)dlsym(RTLD_NEXT,"_ZN2cv6imshowERKSsRKNS_11_InputArrayE");
-#endif
+		// Linux OpenCV 2.4.12: _ZN2cv6imshowERKSsRKNS_11_InputArrayE
+		// OSX OpenCV 2.4.11: __ZN2cv6imshowERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEERKNS_11_InputArrayE
+		#define SYMBOLOSX __ZN2cv6imshowERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEERKNS_11_InputArrayE
+		#define SYMBOLLINUX _ZN2cv6imshowERKSsRKNS_11_InputArrayE
+		
+		#define SYMBOL SYMBOLOSX
+		realimshow = (FX*)dlsym(RTLD_NEXT,SYMBOL);
+		
 		if(!realimshow)
 		{
 			std::cout << "cannot find cv::imshow: use nm -gU sharedlibrary | grep imshow\n";
